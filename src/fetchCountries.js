c646f2213b25fc99d013446855a0f771cf273b55
grant956 <68757554+Psihodel666@users.noreply.github.com>
2022-09-24 16:22:55 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -16,6 +16,7 @@ export async  function fetchCountries(searchName) {
   .then(resp =>{
     return resp.json();
   })
+  
  
     // try {
     //   const resp = await fetch(`${BASE_URL}/name/${searchName}?fields=name,capital,population,flags,languages`)
